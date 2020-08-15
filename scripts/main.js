a8a07e57070474f6cd3011d7d64c7309bea93b1f
Samuel-UC <68044712+Samuel-UC@users.noreply.github.com>
2020-08-14 21:05:20 -0700

    Create main.js

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+const tabs = {
+    requests: document.querySelector('.tab.requests'),
+    websockets: document.querySelector('.tab.websockets'),
+    scripts: document.querySelector('.tab.scripts'),
+    editor: document.querySelector('.tab.editor'),
+    libraries: document.querySelector('.tab.libraries')
+};
